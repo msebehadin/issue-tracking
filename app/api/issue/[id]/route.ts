62f2373f4aa78fe928017db1afbffd4bd48a0517
mohammed <hp@mohammed>
2025-08-31 22:21:41 +0300

    making the update the api

---
+++ app/api/issue/[id]/route.ts
@@ -0,0 +1,23 @@
+import { issueSchema } from "@/app/validationSchema";
+import { prisma } from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success)
+    NextResponse.json(validation.error.issues, { status: 400 });
+ const issue=await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+ });
+    const updateIssue = await prisma.issue.update({
+        where: { id: issue?.id },
+        data: {
+            title: issue?.title,
+            description:issue?.description
+        }
+    })
+}

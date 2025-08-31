bf53adafd6cd19091ff06bd21994bcd47ba00740
mohammed <hp@mohammed>
2025-08-31 11:05:27 +0300

    creating loading page

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingPage = () => {
+  return (
+     <div className="h-12 w-12 border-2 border-t-2 border-black rounded-full animate-spin align-text-center"></div>
+  )
+}
+
+export default LoadingPage

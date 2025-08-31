38b8eae854ad2883e9d74abde95d9dbcacdabe28
mohammed <hp@mohammed>
2025-08-31 11:33:33 +0300

    creating the edit btn

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,25 +1,43 @@
-import { prisma } from "@/prisma/client"
+import { prisma } from "@/prisma/client";
+import Link from "next/link";
+import { BiEdit } from "react-icons/bi";
 
 interface Props {
-  params: { id: string }
+  params: { id: string };
 }
 
 const Page = async ({ params }: Props) => {
   const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) }
-  })
+    where: { id: parseInt(params.id) },
+  });
 
   if (!issue) {
-    return <div>Issue not found.</div>
+    return <div>Issue not found.</div>;
   }
 
   return (
-    <div>
-      <p><strong>Title:</strong> {issue.title}</p>
-      <p><strong>Description:</strong> {issue.description}</p>
-      <p><strong>Created At:</strong> {new Date(issue.createdAt).toLocaleDateString()}</p>
-    </div>
-  )
-}
+    <>
+      <div>
+        <p>
+          <strong>Title:</strong> {issue.title}
+        </p>
+        <p>
+          <strong>Description:</strong> {issue.description}
+        </p>
+        <p>
+          <strong>Created At:</strong>{" "}
+          {new Date(issue.createdAt).toLocaleDateString()}
+        </p>
+      </div>
+      <div>
+        <Link href={`/issues/${issue.id}/edit`}>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-small py-2 px-4 rounded-md transition duration-200 flex align-middle justify-center">
+            <BiEdit  />Edit Issue
+          </button>
+        </Link>
+      </div>
+    </>
+  );
+};
 
-export default Page
+export default Page;
